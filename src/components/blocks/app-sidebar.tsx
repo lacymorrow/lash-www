67a286a66db61d818f0a81a63c4a5ad6175eb612
Lacy Morrow <me@lacymorrow.com>
2025-08-17 17:29:38 -0400

    auto commit

--- src/components/blocks/app-sidebar.tsx
+++ src/components/blocks/app-sidebar.tsx
@@ -1,6 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
-import { ProjectsList } from "@/app/(app)/(dashboard)/projects/_components/projects-list";
 import { NavUser } from "@/components/blocks/nav-user";
 import { TeamSwitcher } from "@/components/blocks/team-switcher";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -36,7 +35,7 @@ const appSidebarVariants = cva("", {
 
 interface AppSidebarProps
 	extends Omit<React.ComponentProps<typeof Sidebar>, "variant">,
-		VariantProps<typeof appSidebarVariants> {
+	VariantProps<typeof appSidebarVariants> {
 	variant?: "inset" | "floating" | "sidebar";
 	size?: "default" | "sm" | "lg";
 }
@@ -59,7 +58,6 @@ export const AppSidebar = React.forwardRef<HTMLDivElement, AppSidebarProps>(
 					<SidebarContent>
 						<ScrollArea className="[&>div>div]:!block">
 							<NavMain />
-							<ProjectsList />
 						</ScrollArea>
 					</SidebarContent>
 					<SidebarFooter className="p-2">
