ad14b2dcfbf4d38a87023174c27eee0d30ff2138
Lacy Morrow <me@lacymorrow.com>
2026-03-09 00:06:50 -0400

    auto commit

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,7 +42,7 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, asChild = false, noHaptics = false, onClick, ...props }, ref) => {
+	({ className, variant, size, asChild = false, noHaptics: _noHaptics = false, ...props }, ref) => {
 		const Comp = asChild ? SlotPrimitive.Slot : "button";
 		return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
 	}
